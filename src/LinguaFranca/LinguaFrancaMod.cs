d069a51ff1c64b842b3158b63ebc44f5ffc53149
tomat <xxlennygamerxx@gmail.com>
2024-01-04 19:49:46 -0800

    Set up projects.

---
+++ src/LinguaFranca/LinguaFrancaMod.cs
@@ -0,0 +1,7 @@
+﻿using JetBrains.Annotations;
+using Terraria.ModLoader;
+
+namespace TeamCatalyst.LinguaFranca;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+public sealed class LinguaFrancaMod : Mod { }
